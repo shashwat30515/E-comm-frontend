65bdc780353c9e5ba294b1316c6a3f1d5569a716
shashwat30515 <166651163+shashwat30515@users.noreply.github.com>
2025-11-19 00:56:34 +0530

    Update axios.jsx

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: " https://e-comm-backend-oo4p.onrender.com/api",
+  baseURL: "https://e-comm-backend-oo4p.onrender.com/api",
 });
 delete API.defaults.headers.common["Authorization"];
 export default API;
