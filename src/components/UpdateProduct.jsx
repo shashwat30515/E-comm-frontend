2c5c0976af0bfee1a2193b9ae3bffd191d781b66
shashwat30515 <166651163+shashwat30515@users.noreply.github.com>
2025-11-19 01:01:35 +0530

    Remove extra spaces from API URLs in UpdateProduct

--- src/components/UpdateProduct.jsx
+++ src/components/UpdateProduct.jsx
@@ -22,13 +22,13 @@ const UpdateProduct = () => {
     const fetchProduct = async () => {
       try {
         const response = await axios.get(
-          ` https://e-comm-backend-oo4p.onrender.com/api/product/${id}`
+          `https://e-comm-backend-oo4p.onrender.com/api/product/${id}`
         );
 
         setProduct(response.data);
       
         const responseImage = await axios.get(
-          ` https://e-comm-backend-oo4p.onrender.com/api/product/${id}/image`,
+          `https://e-comm-backend-oo4p.onrender.com/api/product/${id}/image`,
           { responseType: "blob" }
         );
        const imageFile = await converUrlToFile(responseImage.data,response.data.imageName)
@@ -67,7 +67,7 @@ const UpdateProduct = () => {
 
   console.log("formData : ", updatedProduct)
     axios
-      .put(` https://e-comm-backend-oo4p.onrender.com/api/product/${id}`, updatedProduct, {
+      .put(`https://e-comm-backend-oo4p.onrender.com/api/product/${id}`, updatedProduct, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
